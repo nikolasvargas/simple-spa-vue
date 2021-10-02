c47aef41a238053d4a7ae56c4620a95db3947b69
nikolasvargas <vargasnikolass@gmail.com>
2021-10-01 21:11:15 -0300

    init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <li v-for="nome in nomes" :key="nome">
+      {{nome}}
+    </li>
+  </div>
+</template>
+
+<script>
+export default {
+  // função que retorna dados para o template.
+  data() {
+    return {
+      title: "simple-spa-vue",
+      nomes: [
+        'nikolas',
+        'vargas'
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import Vue from 'vue' //global vue object
+import App from './App.vue'
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
