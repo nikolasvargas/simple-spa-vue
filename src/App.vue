af7d8301e008033636008019fe5fcc207d798470
nikolasvargas <vargasnikolass@gmail.com>
2021-10-02 22:15:07 -0300

    new components and requests

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,63 @@
 <template>
-  <div>
-    <h1>{{ title }}</h1>
-    <li v-for="nome in nomes" :key="nome">
-      {{nome}}
-    </li>
+  <div class="tbody">
+    <h1 class="center">{{ title }}</h1>
+    <!-- v-on:input data-binding é broken em... -->
+    <input type="search" class="input-filter" placeholder="type whatever you want" v-on:input="filterInput = $event.target.value">
+    <ul class="image-list">
+      <li class="image-list-item" v-for="image in images" :key="image.id">
+        <panel :title="image.titulo">
+          <img class="image-content" :src="image.url" :alt="image.titulo">
+        </panel>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import Panel from './components/shared/panel/Panel.vue';
+
 export default {
-  // função que retorna dados para o template.
+
+  components: {
+    panel: Panel
+  },
+
   data() {
     return {
       title: "simple-spa-vue",
-      nomes: [
-        'nikolas',
-        'vargas'
-      ]
+      images: [],
+      filterInput: ''
     }
+  },
+  created() {
+    axios.get('http://localhost:3000/v1/fotos')
+      .then(res => res.data)
+      .then(images => this.images = images, err => console.log(err));
   }
 }
 </script>
 
 <style>
+  .center {
+    text-align: center;
+  }
+
+  .tbody {
+    font-family: Helvetica, sans-serif;
+    margin: 0 auto;
+    width: 96%;
+  }
+
+  .image-list {
+    list-style: none;
+  }
+
+  .image-list .image-list-item {
+    display: inline-block;
+  }
+
+  .image-content {
+    width: 100%;
+  }
 </style>
